850c5b716a58b492162e4896475619e5fb1374ec
“jiegiser” <changjie@jouav.com>
2022-10-08 09:49:17 +0800

    Fix: 修改 JNA 示例

--- JNA/java/demo/src/main/java/com/sun/jna/App.java
+++ JNA/java/demo/src/main/java/com/sun/jna/App.java
@@ -8,6 +8,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         int result = CLibrary.INSTANTCE.sayHello(2, 3);
-        System.out.println("sayHello的放回结果:" + result);
+        System.out.println("sayHello的返回结果:" + result);
     }
 }
